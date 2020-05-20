9759fe9d4d7a035aee6aee2d71d34ba8faa64b77
laurakirkham <kirkham.laura@hotmail.com>
2020-05-20 16:50:56 -0400

    added image to product details, however empty array issue persists on refresh

--- src/Components/ProductDetails.js
+++ src/Components/ProductDetails.js
@@ -15,7 +15,7 @@ const ProductDetails = (props) => {
 // const filteredApiData = matchedProduct ? matchedProduct.fields : null;
 
 
-const filteredApiData = apiData.find(value => value.id ==id);
+const filteredApiData = apiData.length >= 0 && apiData.find(value => value.id ==id);
 console.log(filteredApiData.fields);
 console.log(filteredApiData.fields.name);
 
@@ -24,6 +24,9 @@ render()
         <div>
           <h1>This is the page for product id {id}</h1>
           <h2>product name: {filteredApiData.fields.name}</h2>
+          <h2>heres an image!</h2>
+          <p>{filteredApiData.fields.description}</p>
+          <img src={filteredApiData.fields.image[0].url} alt={filteredApiData.fields.name} />
         </div>
     );
     }
